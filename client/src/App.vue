9cb6d696c98fbe1a925d310ef397e1fbb618d941
Peupty <melvillewatford@gmail.com>
2020-01-29 15:04:07 +0100

    added animated background

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,4 +8,33 @@
 export default {};
 </script>
 
-<style></style>
+<style lang="scss">
+body {
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+  background: radial-gradient(#829e34, #852d52, #254a81);
+  background-size: 400% 400%;
+  animation: Gradient 20s ease-in-out infinite;
+}
+
+@keyframes Gradient {
+  0% {
+    background-position: 50% 10%;
+  }
+  25% {
+    background-position: 90% 50%;
+  }
+  50% {
+    background-position: 50% 90%;
+  }
+  75% {
+    background-position: 10% 50%;
+  }
+  100% {
+    background-position: 50% 10%;
+  }
+}
+</style>
