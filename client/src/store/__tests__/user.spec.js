611d111b7e7bbef658536fe8260792022d763679
Peupty <melvillewatford@gmail.com>
2020-02-05 16:57:01 +0100

    testing user module

---
+++ client/src/store/__tests__/user.spec.js
@@ -0,0 +1,41 @@
+import { moduleMocker } from "../index";
+import user from "../user/index";
+import { defaultState } from "../user/state";
+import store from "../index";
+
+// let store;
+
+beforeEach(() => {
+  // store = moduleMocker(user, "user");
+});
+
+describe("user", () => {
+  describe("state", () => {
+    test("should match schema", () => {
+      const schema = defaultState();
+      expect(store.state.user).toEqual(schema);
+    });
+  });
+
+  describe("mutations", () => {
+    describe("RESET_STATE", () => {
+      test("should reset the state to default", () => {
+        store.commit("user/RESET_STATE");
+
+        expect(store.state.user).toEqual(defaultState());
+      });
+    });
+
+    describe("SET_USER", () => {
+      test("should set the user", () => {
+        const user = {
+          name: "John",
+          email: "john@doe.com"
+        };
+        store.commit("user/SET_USER", user);
+
+        expect(store.state.user).toMatchObject(user);
+      });
+    });
+  });
+});
