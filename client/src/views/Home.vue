83be4b9515776dc16998a5d87b70da83112e1272
Peupty <melvillewatford@gmail.com>
2020-02-05 16:59:00 +0100

    created basic home vue, with mapped user data

---
+++ client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">{{ user }}</div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState({ user: state => state.user })
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
