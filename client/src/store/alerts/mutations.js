144376d06f77f47510454b4119c5682a6ea633b8
Peupty <melvillewatford@gmail.com>
2020-02-08 23:01:51 +0100

    added ADD, REMOVE

--- client/src/store/alerts/mutations.js
+++ client/src/store/alerts/mutations.js
@@ -3,5 +3,12 @@ import { defaultState } from "./state";
 export const mutations = {
   RESET_STATE(state) {
     Object.assign(state, defaultState());
+  },
+  ADD(state, alert) {
+    state.content.push(alert);
+  },
+  REMOVE(state, alert) {
+    const index = state.content.indexOf(alert);
+    state.content.splice(index, 1);
   }
 };
