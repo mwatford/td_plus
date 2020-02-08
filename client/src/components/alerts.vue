197b007866457879899a0539af9aa4af0dcac65b
Peupty <melvillewatford@gmail.com>
2020-02-09 00:43:15 +0100

    created component with styling

---
+++ client/src/components/alerts.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="alerts">
+    <div
+      v-for="(alert, index) in alerts"
+      :key="index"
+      :class="['alert', `alert--${alert.type}`]"
+    >
+      <p>
+        {{ alert.message }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+    ...mapState({
+      alerts: state => state.alerts.content
+    })
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.alerts {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  bottom: 20px;
+  right: 20px;
+}
+.alert {
+  height: 120px;
+  width: 220px;
+  margin-top: 20px;
+  background: #000000a2;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+
+  p {
+    text-align: center;
+  }
+
+  &--error {
+    background: red;
+  }
+  &--success {
+    background: green;
+  }
+  &--warning {
+    background: orange;
+  }
+}
+</style>
