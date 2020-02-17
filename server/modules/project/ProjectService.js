59bfecafac48e0ed3f7d87a0c69949bb51dd7c31
Peupty <melvillewatford@gmail.com>
2020-02-17 23:39:19 +0100

    created projectService

---
+++ server/modules/project/ProjectService.js
@@ -0,0 +1,46 @@
+const createProject = Project => data => {
+  if (!data) {
+    throw new Error("Data was not passed!");
+  }
+
+  const project = new Project(data);
+
+  return project.save();
+};
+
+const find = Project => id => {
+  if (!id) {
+    throw new Error("Id was not provided!");
+  }
+
+  return Project.findById(id);
+};
+
+const authenticate = (project, userId) => {
+  if (!userId) {
+    throw new Error("argument was not passed: userId");
+  }
+  if (!project) {
+    throw new Error("argument was not passed: project");
+  }
+
+  return Boolean(project.members.find(el => el === userId));
+};
+
+const addMember = (project, adminId, userId) => {
+  if (project.admin === adminId) {
+    project.members.push(userId);
+    return project.save();
+  } else {
+    throw new Error("Unauthorized");
+  }
+};
+
+module.exports = Project => {
+  return {
+    createProject: createProject(Project),
+    find: find(Project),
+    authenticate: authenticate,
+    addMember: addMember
+  };
+};
