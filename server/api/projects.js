63170d47a8d00813976b464eff2ac26d11d477c6
Peupty <melvillewatford@gmail.com>
2020-02-19 01:39:21 +0100

    added new api routes

--- server/api/projects.js
+++ server/api/projects.js
@@ -10,7 +10,8 @@ router.post("/projects/create", authenticate, async (req, res) => {
     if (admin) {
       const project = await projectService.createProject({
         admin: admin._id,
-        members: [admin._id]
+        members: [admin._id, ...req.body.project.members],
+        name: req.body.project.name
       });
 
       admin.projects.push(project._id);
@@ -20,6 +21,32 @@ router.post("/projects/create", authenticate, async (req, res) => {
     }
   } catch (e) {
     console.log(e);
+  }
+});
+
+router.get("/projects/all/:id", authenticate, async (req, res) => {
+  const id = req.params.id;
+
+  const user = await userService.findById(id);
+  const projectIDs = user.projects;
+
+  const projects = await projectService.findMany(projectIDs);
+
+  res.send(projects);
+});
+
+router.get("/projects/:id/:userId", authenticate, async (req, res) => {
+  const project = await projectService.find(req.params.id);
+
+  const isAuthenticated = await projectService.authenticate(
+    project,
+    req.params.userId
+  );
+
+  if (isAuthenticated) {
+    return res.send(project);
+  } else {
+    res.status(403).send("Sorry! You can't see that.");
   }
 });
 
