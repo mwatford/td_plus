a152d7396747bdef7f0a6d2cef0d8be9aef6f56e
Peupty <melvillewatford@gmail.com>
2020-02-19 01:38:54 +0100

    added fetchData method

---
+++ client/src/views/Project.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="project">
+    <p>ID = {{ $route.params.id }}</p>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import axios from "axios";
+
+export default {
+  computed: {
+    ...mapState({
+      user: state => state.user,
+      token: state => state.auth.token
+    })
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    fetchData() {
+      return axios({
+        method: "get",
+        url: `/api/projects/${this.$route.params.id}/${this.user._id}`,
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+          "Content-Type": "application/json"
+        }
+      }).catch(err => {
+        this.$store.dispatch("alerts/display", {
+          message: err.response.data,
+          type: "error"
+        });
+        this.$router.push({ name: "home" });
+      });
+    }
+  },
+  mounted() {
+    this.fetchData();
+  }
+};
+</script>
+
+<style></style>
