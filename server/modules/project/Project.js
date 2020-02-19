971ae19942c7ea2137cbb751676c675c6c4c1809
Peupty <melvillewatford@gmail.com>
2020-02-19 01:40:55 +0100

    updated schema with a name field

--- server/modules/project/Project.js
+++ server/modules/project/Project.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ProjectSchema = new Schema({
+  name: { type: String, required: true, default: "" },
   admin: String,
   members: [],
   toDo: [],
