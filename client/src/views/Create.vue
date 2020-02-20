4a8573e3e95a9117044a98a2ef3f8ec908c223f9
Peupty <melvillewatford@gmail.com>
2020-02-20 04:49:23 +0100

    Members and Invite lists added addUser, removeUser added fetchUsers added styles added

--- client/src/views/Create.vue
+++ client/src/views/Create.vue
@@ -1,19 +1,60 @@
 <template>
-  <form class="create">
+  <div class="create row">
     <form action="" class="form" @submit.prevent>
-      <input
-        type="text"
-        name="name"
-        placeholder="project name"
-        v-model="project.name"
-        class="input"
-      />
+      <ul class="col form members" v-if="memberList.length">
+        <h3>Members</h3>
+        <li
+          v-for="suggestion in memberList"
+          :key="suggestion._id"
+          @click="removeUser(suggestion)"
+          class="form__user col"
+          title="Remove"
+        >
+          <h4>{{ suggestion.name }}</h4>
+          <h5>
+            {{ suggestion.email }}
+          </h5>
+        </li>
+      </ul>
+      <ul class="col form suggestions" v-if="suggestions.length">
+        <h3>Invite</h3>
+        <li
+          v-for="suggestion in suggestions"
+          :key="suggestion._id"
+          @click="addUser(suggestion)"
+          class="form__user col"
+          title="Invite"
+        >
+          <h4>{{ suggestion.name }}</h4>
+          <h5>
+            {{ suggestion.email }}
+          </h5>
+        </li>
+      </ul>
+      <div class="col">
+        <input
+          type="text"
+          name="name"
+          placeholder="project name"
+          v-model="project.name"
+          class="input"
+        />
+        <div class="row">
+          <input
+            type="text"
+            class="input"
+            name="invite"
+            v-model="member"
+            placeholder="invite people"
+          />
+        </div>
+      </div>
       <div class="row">
         <button class="button" @click="create">Create</button>
         <button class="button" @click="cancel">Back</button>
       </div>
     </form>
-  </form>
+  </div>
 </template>
 
 <script>
@@ -23,6 +64,9 @@ import { mapState } from "vuex";
 export default {
   data() {
     return {
+      member: "",
+      responseList: [],
+      memberList: [],
       project: {
         name,
         members: []
@@ -33,9 +77,29 @@ export default {
     ...mapState({
       user: state => state.user,
       token: state => state.auth.token
-    })
+    }),
+    suggestions() {
+      const suggestions = this.responseList.filter(
+        el => el._id !== this.user._id
+      );
+
+      this.memberList.forEach(el => {
+        const element = suggestions.find(elem => elem._id === el._id);
+        const index = suggestions.indexOf(element);
+        suggestions.splice(index, 1);
+      });
+      return suggestions;
+    }
   },
   methods: {
+    addUser(user) {
+      this.project.members.push(user._id);
+      this.memberList.push(user);
+    },
+    removeUser(index) {
+      this.memberList.splice(index, 1);
+      this.project.members.splice(index, 1);
+    },
     create() {
       axios({
         method: "post",
@@ -59,9 +123,27 @@ export default {
           this.$router.push({ name: "home" });
         });
     },
-    addMember() {},
     cancel() {
       this.$router.go(-1);
+    },
+    fetchUsers(search) {
+      this.$http({
+        method: "get",
+        url: `/api/users/search/${search}`,
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+          "Content-Type": "application/json"
+        }
+      }).then(response => {
+        this.responseList = response.data;
+      });
+    }
+  },
+  watch: {
+    member(n) {
+      if (this.member.length > 3) {
+        this.fetchUsers(n);
+      }
     }
   }
 };
@@ -72,5 +154,41 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+.form {
+  position: relative;
+
+  h2 {
+    margin-bottom: 2px;
+  }
+
+  &__user {
+    cursor: pointer;
+    margin: 2px auto;
+    width: 100%;
+    border-radius: 4px;
+    padding: 8px;
+    transition: all 0.2s ease;
+
+    &:hover {
+      box-shadow: 0 0 6px 0px #fff;
+      background: #000000a2;
+    }
+  }
+}
+.suggestions,
+.members {
+  position: absolute;
+  justify-content: flex-start;
+  top: 0;
+  height: 100%;
+}
+.suggestions {
+  right: 100%;
+  margin-right: 40px;
+}
+.members {
+  left: 100%;
+  margin-left: 40px;
 }
 </style>
