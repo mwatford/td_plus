481f78dad4925a2d1ce71037c126505fb597a730
Peupty <melvillewatford@gmail.com>
2020-02-20 13:50:57 +0100

    added conditional rendering based on loading value

--- client/src/views/Project.vue
+++ client/src/views/Project.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="project">
-    <p>ID = {{ $route.params.id }}</p>
+  <div class="asd" v-if="loading">
+    loading data
+  </div>
+  <div class="project" v-else>
+    <ul class="col">
+      <h3>To Do</h3>
+      <li v-for="task in project.toDo" :key="task._id"></li>
+    </ul>
   </div>
 </template>
 
@@ -9,31 +15,39 @@ import { mapState } from "vuex";
 import axios from "axios";
 
 export default {
+  data() {
+    return {
+      project: null,
+      loading: true
+    };
+  },
   computed: {
     ...mapState({
       user: state => state.user,
       token: state => state.auth.token
     })
   },
-  data() {
-    return {};
-  },
   methods: {
     fetchData() {
-      axios({
+      this.$http({
         method: "get",
         url: `/api/projects/${this.$route.params.id}`,
         headers: {
           Authorization: `Bearer ${this.token}`,
           "Content-Type": "application/json"
         }
-      }).catch(err => {
-        this.$store.dispatch("alerts/display", {
-          message: err.response.data,
-          type: "error"
+      })
+        .then(({ data }) => {
+          this.project = data;
+          this.loading = false;
+        })
+        .catch(err => {
+          this.$store.dispatch("alerts/display", {
+            message: err.response.data,
+            type: "error"
+          });
+          this.$router.push({ name: "home" });
         });
-        this.$router.push({ name: "home" });
-      });
     }
   },
   mounted() {
