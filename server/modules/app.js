efd074e0de2a52f5bc460a5889b6f70bbcf4f7df
Peupty <melvillewatford@gmail.com>
2020-02-22 05:04:56 +0100

    removed /app endpoint

--- server/modules/app.js
+++ server/modules/app.js
@@ -9,10 +9,6 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../../public/index.html"));
-});
-
-app.get("/app", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../../dist/index.html"));
 });
 
