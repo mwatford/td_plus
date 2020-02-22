4f442bd90db9698d2041153b4c6e0596b67e01a0
Peupty <melvillewatford@gmail.com>
2020-02-22 04:50:54 +0100

    created animation mixin for .box class

---
+++ client/src/mixins/boxAnimations.js
@@ -0,0 +1,36 @@
+function stagger(staggerVal, delay) {
+  if (staggerVal) {
+    return (el, index) => index * delay;
+  }
+  return delay;
+}
+
+export default {
+  methods: {
+    boxEnterAnimation(duration, delay = 100, staggerVal) {
+      if (this.$store.state.app.animations) {
+        this.$anime.remove(".box");
+        return this.$anime({
+          targets: ".box",
+          duration,
+          delay: stagger(staggerVal, delay),
+          easing: "easeOutBack",
+          scale: [0, 1]
+        });
+      }
+      return new Promise(resolve => resolve());
+    },
+    boxExitAnimation(duration, delay = 100, staggerVal) {
+      if (this.$store.state.app.animations) {
+        return this.$anime({
+          targets: ".box",
+          duration,
+          delay: stagger(staggerVal, delay),
+          scale: [1, 0],
+          easing: "easeInBack"
+        }).finished;
+      }
+      return new Promise(resolve => resolve());
+    }
+  }
+};
