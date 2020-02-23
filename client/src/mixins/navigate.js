2b418b191db93102cb72502875828482db6cde5f
Peupty <melvillewatford@gmail.com>
2020-02-23 20:25:04 +0100

    updated mixin to work for different argument types

--- client/src/mixins/navigate.js
+++ client/src/mixins/navigate.js
@@ -1,7 +1,13 @@
 export default {
   methods: {
-    navigate(name) {
-      this.$router.push({ name });
+    navigate(value) {
+      if (typeof value == "string") {
+        this.$router.push({ name: value });
+      } else if (typeof value == "number") {
+        this.$router.go(value);
+      } else {
+        this.$router.push(value);
+      }
     }
   }
 };
