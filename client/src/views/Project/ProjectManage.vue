ad55b651b6513a5c3b16024e8ed064df94b14d5a
Peupty <melvillewatford@gmail.com>
2020-02-23 20:30:56 +0100

    created component auth method added loading component

---
+++ client/src/views/Project/ProjectManage.vue
@@ -0,0 +1,50 @@
+<template>
+  <loading v-if="loading !== 'start'" :state="loading" :size="60"></loading>
+  <div v-else>manage project</div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {
+      loading: "start"
+    };
+  },
+  computed: {
+    ...mapState({ token: state => state.auth.token })
+  },
+  methods: {
+    auth() {
+      this.loading = "loading";
+      this.$http({
+        method: "get",
+        url: `/api/projects/${this.$route.params.id}/admin`,
+        headers: {
+          Authorization: `Bearer ${this.token}`,
+          "Content-Type": "application/json"
+        }
+      })
+        .then(() => {
+          this.loading = "done";
+          setTimeout(() => {
+            this.loading = "start";
+          }, 500);
+        })
+        .catch(e => {
+          this.loading = "failed";
+          this.$store.dispatch("alerts/display", {
+            type: "error",
+            message: e.response.data
+          });
+          this.$router.go(-1);
+        });
+    }
+  },
+  mounted() {
+    this.auth();
+  }
+};
+</script>
+
+<style></style>
