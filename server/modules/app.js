901ece06eb7c24b96589ff7baac6877aa8abf264
Peupty <melvillewatford@gmail.com>
2020-02-24 00:17:13 +0100

    app returns a promise and waits for db connection

--- server/modules/app.js
+++ server/modules/app.js
@@ -4,45 +4,49 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const app = express();
+const app = async () => {
+  const app = express();
 
-app.use(express.json());
+  app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../../dist/index.html"));
-});
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../../dist/index.html"));
+  });
 
-app.use(express.static("dist"));
-app.use(express.static("public"));
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+  app.use(express.static("dist"));
+  app.use(express.static("public"));
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
-const api = require("../api/index");
+  const api = require("../api/index");
 
-app.use("/api", api);
+  app.use("/api", api);
 
-mongoose.Promise = global.Promise;
+  mongoose.Promise = global.Promise;
 
-let mongoPath;
+  let mongoPath;
 
-if (process.env.NODE_ENV !== "production") {
-  mongoPath = "mongodb://localhost/db";
-  mongoose.set("debug", true);
-} else {
-  mongoPath = require("../config/keys").mongodb;
-}
-
-mongoose.connect(
-  mongoPath,
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
-  },
-  () => {
-    console.log("connected to DB", mongoPath);
+  if (process.env.NODE_ENV !== "production") {
+    mongoPath = "mongodb://localhost/db";
+    mongoose.set("debug", true);
+  } else {
+    mongoPath = require("../config/keys").mongodb;
   }
-);
+
+  await mongoose.connect(
+    mongoPath,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false
+    },
+    () => {
+      console.log("connected to DB", mongoPath);
+    }
+  );
+
+  return app;
+};
 
 module.exports = app;
