f10b00150b8bd296e5e7ccae34e9483c437f1fa7
Peupty <melvillewatford@gmail.com>
2020-02-24 00:17:41 +0100

    refactoring: waits for app promise to resolve

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-const app = require("./modules/app");
-
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, console.log(`Server running on port: ${PORT}`));
+(async () => {
+  const app = await require("./modules/app")();
+  app.listen(PORT, console.log(`Server running on port: ${PORT}`));
+})();
