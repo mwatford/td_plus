01082b2372d020acf4c053d7a286f45ae2e21a02
Peupty <melvillewatford@gmail.com>
2020-02-23 23:37:58 +0100

    updates

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -1,13 +1,8 @@
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
+const authConfig = require("./auth_config");
 
 // Set up Auth0 configuration
-const authConfig = {
-  domain: "dev-mg9endvd.eu.auth0.com",
-  clientId: "2mdeCAWlvBM4sdS7disJDhVRLXXUo4hZ",
-  audience: 'https://api.tdp',
-};
-
 // Define middleware that validates incoming bearer tokens
 // using JWKS from dev-mg9endvd.eu.auth0.com
 const checkJwt = jwt({
