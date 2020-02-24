e4960881fdfcbc43d579c0f5b5f480b9866ded73
Peupty <melvillewatford@gmail.com>
2020-02-24 07:00:06 +0100

    added basic animations

---
+++ client/src/mixins/animations.js
@@ -0,0 +1,56 @@
+export const fadeOut = function(duration, delay, easing) {
+  return (el, done) => {
+    return this.$anime({
+      targets: el,
+      opacity: [1, 0],
+      duration,
+      delay: (el, index) => index * delay,
+      easing
+    }).finished.then(done);
+  };
+};
+
+export const fadeIn = function(duration, delay, easing) {
+  return (el, done) => {
+    return this.$anime({
+      targets: el,
+      opacity: [0, 1],
+      duration,
+      delay: (el, index) => index * delay,
+      easing
+    }).finished.then(done);
+  };
+};
+
+export const popUp = function(duration, delay, easing) {
+  return (el, done) => {
+    return this.$anime({
+      targets: el,
+      scale: [0, 1],
+      duration,
+      delay: (el, index) => index * delay,
+      easing
+    }).finished.then(done);
+  };
+};
+
+export const scaleDown = function(duration, delay, easing) {
+  return (el, done) => {
+    return this.$anime({
+      targets: el,
+      scale: [1, 0],
+      duration,
+      delay: (el, index) => index * delay,
+      easing
+    }).finished.then(done);
+  };
+};
+
+export default {
+  methods: {
+    fadeOut,
+    fadeIn,
+    popUp,
+    scaleDown
+  }
+};
