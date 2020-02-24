11109d77a63d83cc7a36e2c8619d0be332969f75
Peupty <melvillewatford@gmail.com>
2020-02-24 06:50:26 +0100

    changed user-details to username component

---
+++ client/src/components/username.vue
@@ -0,0 +1,58 @@
+<template>
+  <form action="" @submit.prevent="chooseName">
+    <input
+      type="text"
+      name=""
+      id=""
+      class="input"
+      placeholder="Choose your name"
+      v-model="name"
+    />
+    <button class="m-auto button">OK</button>
+  </form>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {
+      name: ""
+    };
+  },
+  computed: {
+    ...mapState({
+      token: state => state.auth.token
+    })
+  },
+  methods: {
+    chooseName() {
+      this.$store
+        .dispatch("user/save", {
+          token: this.token,
+          changes: {
+            name: this.name
+          }
+        })
+        .then(response => {
+          const { message, type } = response.data;
+
+          this.$store.dispatch("alerts/display", { message, type });
+
+          if (type === "success") {
+            this.$store.commit("user/SET_USER", { name: this.name });
+            this.$eventBus.$emit("name chosen");
+          }
+        });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+</style>
