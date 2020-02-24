aaea20d650e806b64b4a30676ac4add1bfdb2550
Peupty <melvillewatford@gmail.com>
2020-02-24 06:49:14 +0100

    reworking sign in component

--- client/src/components/sign-in.vue
+++ client/src/components/sign-in.vue
@@ -1,8 +1,7 @@
 <template>
-  <form class="form" @submit.prevent="login">
+  <form @submit.prevent="login">
     <h2 class="header">Sign in</h2>
-    <loading :size="60" :state="state" class="m-auto" v-if="loading"></loading>
-    <button name="submit" class="m-auto button" v-else>
+    <button name="submit" class="m-auto button">
       Sign in
     </button>
   </form>
@@ -12,42 +11,22 @@
 export default {
   data() {
     return {
-      email: "",
-      password: "",
-      loading: false,
-      state: "failed"
+      state: "start"
     };
   },
-  computed: {},
   methods: {
     login() {
-      this.loading = true;
-      this.state = "loading";
-      this.$auth
-        .loginWithPopup()
-        .then(async () => {
-          this.state = "success";
-          const { isAuthenticated, user } = this.$auth;
-          const token = await this.$auth.getTokenSilently();
-          this.$store.commit("auth/SET_TOKEN", token);
-          return this.$store.dispatch("user/fetchUser", {
-            token,
-            email: user.email
-          });
-        })
-        .then(() => {
-          this.loading = false;
-          this.$store.commit("auth/SET_STATUS", true);
-          this.$router.push({ name: "home" });
-        })
-        .catch(err => {
-          this.state = "failed";
-          console.log(err);
-        });
+      this.$eventBus.$emit("signIn");
     }
-  },
-  mounted() {}
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-between;
+}
+</style>
