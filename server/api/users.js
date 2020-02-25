4bbfe72a72831a93550377338a84daa271c53301
Peupty <melvillewatford@gmail.com>
2020-02-25 14:08:50 +0100

    moved handler function to /utils

--- server/api/users.js
+++ server/api/users.js
@@ -3,16 +3,7 @@ const controller = require("../controllers/users/index");
 
 const authenticate = require("../middleware/authentication");
 
-const handler = (promise, params) => async (req, res, next) => {
-  const boundParams = params ? params(req, res, next) : {};
-  try {
-    const { status, data } = await promise(boundParams);
-
-    return res.status(status).send(data);
-  } catch (error) {
-    return res.status(500) && next(error);
-  }
-};
+const handler = require("../utils/handler");
 
 router.use(authenticate);
 
