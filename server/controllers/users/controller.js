099573d5ddf888d5e805bcecdce2d9619db2642f
Peupty <melvillewatford@gmail.com>
2020-02-25 13:56:03 +0100

    refactoring methods

--- server/controllers/users/controller.js
+++ server/controllers/users/controller.js
@@ -1,62 +1,46 @@
-const currentUser = services => async (req, res) => {
-  try {
-    const { userService } = services;
-    const { sub } = req.user;
-    const user = await userService.find(sub);
+const currentUser = services => async ({ sub, email }) => {
+  const { userService } = services;
 
-    if (user) {
-      return res.send(user);
-    } else {
-      const profile = {
-        email: req.body.email,
-        sub
-      };
-      const newUser = await userService.createUser(profile);
-      delete newUser.sub;
-      return res.send(newUser);
-    }
-  } catch (e) {
-    res.sendStatus(500);
+  const user = await userService.find(sub);
+
+  if (user) {
+    return { data: user, status: 200 };
+  } else {
+    const profile = {
+      email,
+      sub
+    };
+    const newUser = await userService.createUser(profile);
+    delete newUser.sub;
+    return { data: newUser, status: 200 };
   }
 };
 
-const userUpdate = services => async (req, res) => {
-  try {
-    const { userService } = services;
-    const { changes } = req.body;
-    const { sub } = req.user;
+const userUpdate = services => async ({ sub, changes }) => {
+  const { userService } = services;
 
-    const user = await userService.find(sub);
-    const updatedUser = await userService.updateUser(user, changes);
+  const user = await userService.find(sub);
+  const updatedUser = await userService.updateUser(user, changes);
 
-    if (updatedUser) {
-      return res.send({
-        message: "Your data has been updated",
-        type: "success",
-        updatedUser
-      });
-    } else {
-      return res.send({
-        message: "Something went wrong, try again later.",
-        type: "error"
-      });
-    }
-  } catch (e) {
-    res.sendStatus(500);
+  if (updatedUser) {
+    return {
+      status: 200,
+      data: updatedUser
+    };
+  } else {
+    return {
+      status: 200
+    };
   }
 };
 
-const searchEmail = services => async (req, res) => {
-  try {
-    const { userService } = services;
-    const email = req.params.email;
-    const regexp = new RegExp(email, "g");
-    const users = await userService.get({ email: regexp }, "_id email name");
+const searchEmail = services => async email => {
+  const { userService } = services;
+  const regexp = new RegExp(email, "g");
 
-    res.send(users);
-  } catch (e) {
-    res.sendStatus(500);
-  }
+  const users = await userService.get({ email: regexp }, "_id email name");
+
+  return users;
 };
 
 module.exports = services => {
