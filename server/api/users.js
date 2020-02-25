34b87f717dd10c5f4cf60cdbf263c5fb729d461c
Peupty <melvillewatford@gmail.com>
2020-02-25 13:56:49 +0100

    added handler function refactored routes to use handler function

--- server/api/users.js
+++ server/api/users.js
@@ -3,12 +3,43 @@ const controller = require("../controllers/users/index");
 
 const authenticate = require("../middleware/authentication");
 
+const handler = (promise, params) => async (req, res, next) => {
+  const boundParams = params ? params(req, res, next) : {};
+  try {
+    const { status, data } = await promise(boundParams);
+
+    return res.status(status).send(data);
+  } catch (error) {
+    return res.status(500) && next(error);
+  }
+};
+
 router.use(authenticate);
 
-router.post("/current", controller.currentUser);
+router.post(
+  "/current",
+  handler(controller.currentUser, (req, res, next) => {
+    return { sub: req.user.sub, email: req.body.email };
+  })
+);
 
-router.put("/current/update", controller.userUpdate);
+router.put(
+  "/current/update",
+  handler(controller.userUpdate, (req, res, next) => {
+    return {
+      sub: req.user.sub,
+      changes: req.body.changes
+    };
+  })
+);
 
-router.get("/search/:email", controller.searchEmail);
+router.get(
+  "/search/:email",
+  handler(controller.searchEmail, (req, res, next) => {
+    return {
+      email: req.params.email
+    };
+  })
+);
 
 module.exports = router;
