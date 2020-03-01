f2e3d3454cca97f874f10d21122265ed56be968d
Peupty <melvillewatford@gmail.com>
2020-03-01 15:14:27 +0100

    added markup added filteredList property mapping state

--- client/src/views/Project/Dashboard.vue
+++ client/src/views/Project/Dashboard.vue
@@ -1,21 +1,29 @@
 <template>
-  <div class="m-auto">
-    <ul class="col">
-      <li>
-        dashboard
-      </li>
-      <li v-for="task in project.toDo" :key="task._id"></li>
+  <div class="m-auto" v-if="project" ref="component">
+    <ul class="col" v-for="list in filteredLists" :key="list.name" ref="list">
+      <h3>
+        {{ list.name }}
+      </h3>
+      <li v-for="task in list.data" :key="task._id">{{ task.name }}</li>
     </ul>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
+
 export default {
   computed: {
     ...mapState({
-      project: state => state.activeProject
-    })
+      project: state => state.activeProject,
+      user: state => state.user
+    }),
+    filteredLists() {
+      return this.project.lists.map(el => {
+        el.data = el.data.filter(task => task.member === this.user._id);
+        return el;
+      });
+    }
   },
   methods: {}
 };
