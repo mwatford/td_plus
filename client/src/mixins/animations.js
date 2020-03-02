0185182932628823c9612088aff718adb6d9fb3d
Peupty <melvillewatford@gmail.com>
2020-03-02 20:45:39 +0100

    updated animations

--- client/src/mixins/animations.js
+++ client/src/mixins/animations.js
@@ -1,49 +1,61 @@
 export const fadeOut = function(duration, delay, easing) {
-  return (el, done) => {
-    return this.$anime({
-      targets: el,
-      opacity: [1, 0],
-      duration,
-      delay: (el, index) => index * delay,
-      easing
-    }).finished.then(done);
-  };
+  if (this.$store.state.app.animations) {
+    return (el, done) => {
+      return this.$anime({
+        targets: el,
+        opacity: [1, 0],
+        duration,
+        delay: (el, index) => index * delay,
+        easing
+      }).finished.then(done);
+    };
+  }
+  return Promise.resolve();
 };
 
 export const fadeIn = function(duration, delay, easing) {
-  return (el, done) => {
-    return this.$anime({
-      targets: el,
-      opacity: [0, 1],
-      duration,
-      delay: (el, index) => index * delay,
-      easing
-    }).finished.then(done);
-  };
+  if (this.$store.state.app.animations) {
+    return (el, done) => {
+      return this.$anime({
+        targets: el,
+        opacity: [0, 1],
+        duration,
+        delay: (el, index) => index * delay,
+        easing
+      }).finished.then(done);
+    };
+  }
+  return Promise.resolve();
 };
 
 export const popUp = function(duration, delay, easing) {
-  return (el, done) => {
-    return this.$anime({
-      targets: el,
-      scale: [0, 1],
-      duration,
-      delay: (el, index) => index * delay,
-      easing
-    }).finished.then(done);
-  };
+  if (this.$store.state.app.animations) {
+    return (el, done) => {
+      return this.$anime({
+        targets: el,
+        scale: [0, 1],
+        duration,
+        delay: (el, index) => index * delay,
+        easing
+      }).finished.then(done);
+    };
+  }
+  return Promise.resolve();
 };
 
 export const scaleDown = function(duration, delay, easing) {
-  return (el, done) => {
-    return this.$anime({
-      targets: el,
-      scale: [1, 0],
-      duration,
-      delay: (el, index) => index * delay,
-      easing
-    }).finished.then(done);
-  };
+  if (this.$store.state.app.animations) {
+    return (el, done) => {
+      return this.$anime({
+        targets: el,
+        scale: [1, 0],
+        duration,
+        delay: (el, index) => index * delay,
+        easing
+      }).finished.then(done);
+    };
+  }
+  return Promise.resolve();
 };
 
 export default {
