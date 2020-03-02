2ffa531138ebf3b75ad9f9dbfe252663c91c78ed
Peupty <melvillewatford@gmail.com>
2020-03-02 04:58:54 +0100

    added test suites

---
+++ client/src/views/__tests__/unit/Dashboard-vue.spec.js
@@ -0,0 +1,62 @@
+import Component from "../../Project/Dashboard.vue";
+import { createLocalVue, mount, shallowMount } from "@vue/test-utils";
+import activeProject from "activeProject";
+import user from "user";
+
+let localVue = createLocalVue();
+
+describe("Component", () => {
+  const wrapper = shallowMount(Component, {
+    localVue,
+    computed: {
+      project: () => undefined
+    }
+  });
+
+  describe("does not render", () => {
+    test("component if mapState returns nothing", () => {
+      const actual = wrapper.find({ ref: "component" }).exists();
+
+      expect(actual).toBeFalsy();
+    });
+  });
+
+  describe("renders", () => {
+    const wrapper = shallowMount(Component, {
+      computed: {
+        project: () => activeProject,
+        user: () => user
+      },
+      localVue
+    });
+
+    test("correct number of projects lists if mapState returns value", () => {
+      expect(wrapper.findAll({ ref: "list" }).length).toEqual(2);
+    });
+
+    test("lists with correct names", () => {
+      const actual = wrapper.findAll({ ref: "list" });
+
+      expect(actual.length).toEqual(2);
+      expect(
+        actual
+          .at(0)
+          .find("h3")
+          .text()
+      ).toEqual("To Do");
+      expect(
+        actual
+          .at(1)
+          .find("h3")
+          .text()
+      ).toEqual("Done");
+    });
+
+    test("filtered user tasks correctly", () => {
+      const actual = wrapper.find({ ref: "list" }).findAll("li");
+
+      expect(actual.length).toEqual(1);
+      expect(actual.at(0).text()).toEqual("test task name");
+    });
+  });
+});
