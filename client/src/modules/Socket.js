6877acecfb4b448aa005e97e8aa2f8419ab088f3
Peupty <melvillewatford@gmail.com>
2020-03-16 23:37:45 +0100

    created VueSocket class with wrapping methods

---
+++ client/src/modules/Socket.js
@@ -0,0 +1,28 @@
+export default class VueSocket {
+  constructor(io) {
+    this.socket = null;
+    this.io = io;
+  }
+  connect(opt, fn) {
+    if (!this.socket) {
+      this.socket = this.io("localhost:8000", opt);
+    }
+  }
+  close() {
+    if (this.socket) {
+      this.socket.close();
+      this.socket = null;
+    }
+  }
+  emit(name, data) {
+    if (this.socket) {
+      this.socket.emit(name, data);
+    }
+  }
+  on(name, fn) {
+    this.socket.on(name, fn);
+  }
+  get instance() {
+    return this.socket;
+  }
+}
