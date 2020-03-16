611aa4cee93540a8c9037a10a9d5f6ef5cc537a8
Peupty <melvillewatford@gmail.com>
2020-03-16 23:54:51 +0100

    refactoring

--- server/index.js
+++ server/index.js
@@ -7,12 +7,5 @@ const PORT = process.env.PORT || 8000;
 
   http.listen(PORT, console.log(`Server running on port: ${PORT}`));
 
-  const io = require("socket.io")(http);
-
-  io.on("connection", socket => {
-    console.log("asd");
-    socket.on("disconnect", () => {
-      console.log("dc");
-    });
-  });
+  require("./controllers/liveProject/index")(http);
 })();
