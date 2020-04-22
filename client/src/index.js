b3766dfa7b5730f31a0eb86767d49ce5715068d1
Peupty <melvillewatford@gmail.com>
2020-04-22 20:12:06 +0200

    add global mixin 'alert'

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,23 @@
-import Vue from "vue";
-import App from "./App.vue";
-import store from "./store/index";
-import router from "./router/index";
-import axios from "axios";
-import loading from "./components/loading.vue";
-import anime from "animejs";
-import io from "socket.io-client";
-import VueChatScroll from "vue-chat-scroll";
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store/index';
+import router from './router/index';
+import axios from 'axios';
+import loading from './components/loading.vue';
+import anime from 'animejs';
+import io from 'socket.io-client';
+import VueChatScroll from 'vue-chat-scroll';
+import alert from './mixins/alert';
 
-Vue.component("loading", loading);
+Vue.mixin(alert)
 
-import { domain, clientId, audience } from "../../auth_config.json";
+Vue.component('loading', loading);
+
+import { domain, clientId, audience } from '../../auth_config.json';
 
 // Import the plugin here
-import { Auth0Plugin } from "./auth/index.js";
-import VueSocket from "./modules/Socket";
+import { Auth0Plugin } from './auth/index.js';
+import VueSocket from './modules/Socket';
 
 // Install the authentication plugin here
 Vue.use(Auth0Plugin, {
@@ -22,8 +25,8 @@ Vue.use(Auth0Plugin, {
   clientId,
   audience,
   onRedirectCallback: appState => {
-    router.push({ name: "home" });
-  }
+    router.push({ name: 'home' });
+  },
 });
 
 Vue.use(VueChatScroll);
@@ -38,5 +41,5 @@ Vue.config.productionTip = false;
 new Vue({
   store,
   router,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
