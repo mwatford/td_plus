7634bc45d09c5fcceb5c259510b757fefd208b7c
Peupty <melvillewatford@gmail.com>
2020-04-23 15:04:50 +0200

    create a module with icons for app-icon component

---
+++ client/src/components/icons/index.js
@@ -0,0 +1,13 @@
+import home from './home.vue';
+import signIn from './sign-in.vue';
+import power from './power.vue';
+import user from './user.vue';
+import reload from './reload.vue';
+
+export default {
+  home,
+  'sign-in': signIn,
+  power,
+  user,
+  reload,
+};
