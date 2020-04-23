8f4c3d05bfe3b5d4d9ffaae2d3c9392338ef4f7f
Peupty <melvillewatford@gmail.com>
2020-04-23 16:50:24 +0200

    lint

--- client/src/components/username.vue
+++ client/src/components/username.vue
@@ -8,40 +8,40 @@
       placeholder="Choose your name"
       v-model="name"
     />
-    <button class="m-auto button" name="submit">OK</button>
+    <button class="button m-auto" name="submit">OK</button>
   </form>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState } from 'vuex';
 export default {
   data() {
     return {
-      name: ""
+      name: '',
     };
   },
   computed: {
     ...mapState({
-      token: state => state.auth.token
-    })
+      token: state => state.auth.token,
+    }),
   },
   methods: {
     chooseName() {
       this.$store
-        .dispatch("user/save", {
+        .dispatch('user/save', {
           token: this.token,
           changes: {
-            name: this.name
-          }
+            name: this.name,
+          },
         })
         .then(response => {
           const { message, type } = response.data;
 
-          this.$store.commit("user/SET_USER", { name: this.name });
-          this.$eventBus.$emit("name chosen");
+          this.$store.commit('user/SET_USER', { name: this.name });
+          this.$eventBus.$emit('name chosen');
         });
-    }
-  }
+    },
+  },
 };
 </script>
 
