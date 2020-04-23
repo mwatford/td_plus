26defb64c2e8a7e330d8664ca305742ef0c829c7
Peupty <melvillewatford@gmail.com>
2020-04-23 12:45:28 +0200

    add delete method, lint

--- server/controllers/users/controller.js
+++ server/controllers/users/controller.js
@@ -6,7 +6,7 @@ const currentUser = services => async ({ sub, email }) => {
   if (!user) {
     const profile = {
       email,
-      sub
+      sub,
     };
 
     const newUser = await userService.createUser(profile);
@@ -25,27 +25,52 @@ const userUpdate = services => async ({ sub, changes }) => {
 
   if (!updatedUser) {
     return {
-      status: 200
+      status: 200,
     };
   }
   return {
     status: 200,
-    data: updatedUser
+    data: updatedUser,
   };
 };
 
 const searchEmail = services => async ({ email }) => {
   const { userService } = services;
-  const regexp = new RegExp(email, "g");
+  const regexp = new RegExp(email, 'g');
 
-  const users = await userService.get({ email: regexp }, "_id email name");
+  const users = await userService.get({ email: regexp }, '_id email name');
   return { status: 200, data: users };
+};
+
+const deleteUser = services => async ({ sub }) => {
+  const { userService, projectService } = services;
+
+  const user = await userService.find(sub);
+
+  const { projects } = user;
+
+  const userProjects = await projectService.findMany(projects);
+
+  await Promise.all(
+    userProjects.map(project => {
+      const member = project.members.find(el => user._id.equals(el.id));
+      const index = project.members.indexOf(member);
+      project.members.splice(index, 1);
+
+      return project.save();
+    })
+  );
+
+  userService.delete(sub);
+
+  return { status: 200 };
 };
 
 module.exports = services => {
   return {
     currentUser: currentUser(services),
     userUpdate: userUpdate(services),
-    searchEmail: searchEmail(services)
+    searchEmail: searchEmail(services),
+    delete: deleteUser(services),
   };
 };
