bfab20e2bb8b27b3b5c016d92c7a54eb9868e9a4
Peupty <melvillewatford@gmail.com>
2020-04-24 16:22:28 +0200

    lint

--- client/src/store/projects/actions.js
+++ client/src/store/projects/actions.js
@@ -2,15 +2,14 @@ export const actions = requestModule => {
   return {
     fetchProjects({ commit }, { token, id }) {
       return requestModule({
-        method: "get",
+        method: 'get',
         url: `/api/projects/all/${id}`,
         headers: {
           Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json"
-        }
+        },
       }).then(response => {
-        commit("SET_PROJECTS", response.data);
+        commit('SET_PROJECTS', response.data);
       });
-    }
+    },
   };
 };
