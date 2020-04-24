fee2826b17f2f6e8f24810d223c16fd3c8bfe009
Peupty <melvillewatford@gmail.com>
2020-04-24 16:30:29 +0200

    update if statement in mounted, lint

--- client/src/views/Project/Project.vue
+++ client/src/views/Project/Project.vue
@@ -3,24 +3,24 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-import navigate from "../../mixins/navigate";
-import { popUp, scaleDown } from "../../mixins/animations";
-import loader from "../../components/loading.vue";
-import dashboard from "./Dashboard.vue";
-import manage from "./ProjectManage.vue";
-import password from "./password.vue";
+import { mapState } from 'vuex';
+import navigate from '../../mixins/navigate';
+import { popUp, scaleDown } from '../../mixins/animations';
+import loader from '../../components/loading.vue';
+import dashboard from './Dashboard.vue';
+import manage from './ProjectManage.vue';
+import password from './password.vue';
 
 export default {
   data() {
     return {
-      state: "start",
+      state: 'start',
       component: null,
       views: {
         manage,
         password,
-        dashboard
-      }
+        dashboard,
+      },
     };
   },
   computed: {
@@ -29,44 +29,48 @@ export default {
       token: state => state.auth.token,
       project: state => state.activeProject.data,
       enterAnimation() {
-        return this.popUp(300, 0, "easeInSine");
+        return this.popUp(300, 0, 'easeInSine');
       },
       leaveAnimation() {
-        return this.scaleDown(300, 0, "linear");
-      }
-    })
+        return this.scaleDown(300, 0, 'linear');
+      },
+    }),
   },
   methods: {
     popUp,
     scaleDown,
     hideLoader() {
-      this.state = "done";
+      this.state = 'done';
       setTimeout(() => {
-        this.state = "start";
+        this.state = 'start';
       }, 500);
     },
     changeView(view) {
       this.component = this.views[view];
     },
     grantAccess() {
-      this.$eventBus.$emit("fetch data");
+      this.$eventBus.$emit('fetch data');
       this.component = this.views.loader;
-    }
+    },
   },
   mounted() {
-    this.$eventBus.$on("changeView", this.changeView);
-    this.$eventBus.$on("correct password", this.grantAccess);
-    if (!this.project.password || this.user._id === this.project.admin) {
+    this.$eventBus.$on('changeView', this.changeView);
+    this.$eventBus.$on('correct password', this.grantAccess);
+
+    if (
+      this.project &&
+      (!this.project.password || this.user._id === this.project.admin)
+    ) {
       this.component = loader;
-      this.$eventBus.$emit("fetch data");
+      this.$eventBus.$emit('fetch data');
     } else {
       this.component = password;
     }
   },
   beforeDestroy() {
-    this.$eventBus.$off('changeView', this.changeView)
-    this.$eventBus.$off("correct password", this.grantAccess);
-  }
+    this.$eventBus.$off('changeView', this.changeView);
+    this.$eventBus.$off('correct password', this.grantAccess);
+  },
 };
 </script>
 
