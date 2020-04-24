76468bdd2bed4135fa3afe193ef3e18e56df0952
Peupty <melvillewatford@gmail.com>
2020-04-24 16:27:50 +0200

    remove versionKey

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,12 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ProjectSchema = new Schema({
-  name: { type: String, required: true, default: "" },
-  admin: { type: String, required: true },
-  members: [],
-  lists: [{ name: String, data: [{ name: String, member: String }] }],
-  password: String
-});
+const ProjectSchema = new Schema(
+  {
+    name: { type: String, required: true, default: '' },
+    admin: { type: String, required: true },
+    members: [],
+    lists: [{ name: String, data: [{ name: String, member: String }] }],
+    password: String,
+  },
+  {
+    versionKey: false,
+  }
+);
 
-module.exports = mongoose.model("Project", ProjectSchema);
+module.exports = mongoose.model('Project', ProjectSchema);
