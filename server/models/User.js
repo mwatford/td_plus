c44623a35bb29f99e49ccc3a75b794d5e99bc3f9
Peupty <melvillewatford@gmail.com>
2020-04-26 16:20:59 +0200

    lint

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   sub: { type: String, required: true },
   email: String,
   password: String,
-  name: { type: String, default: "" },
+  name: { type: String, default: '' },
   projects: [],
-  friends: [],
   loginStrategy: String,
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
