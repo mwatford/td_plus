fb5a44232776cf16329eb347ff164b7ad9f9bacf
Peupty <melvillewatford@gmail.com>
2020-04-27 00:35:39 +0200

    lint

--- client/src/store/user/actions.js
+++ client/src/store/user/actions.js
@@ -2,37 +2,37 @@ export const actions = requestModule => {
   return {
     fetchUser({ commit }, { token, email }) {
       return requestModule({
-        method: "post",
-        url: "/api/users/current",
+        method: 'post',
+        url: '/api/users/current',
         headers: {
           Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json',
         },
         data: {
-          email
-        }
+          email,
+        },
       }).then(response => {
-        commit("SET_USER", response.data);
+        commit('SET_USER', response.data);
       });
     },
     resetState({ commit }) {
-      commit("RESET_STATE");
+      commit('RESET_STATE');
     },
     logout({ commit }) {
-      commit("RESET_STATE");
+      commit('RESET_STATE');
     },
-    save({ commit }, { changes, token }) {
+    save({ commit }, { changes, token, id }) {
       return requestModule({
-        method: "put",
-        url: "/api/users/current/update",
+        method: 'put',
+        url: `/api/users/${id}`,
         headers: {
           Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json',
         },
         data: {
-          changes
-        }
+          changes,
+        },
       });
-    }
+    },
   };
 };
