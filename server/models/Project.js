e1a38e7c30953190da054042fd261356c7e0f636
Peupty <melvillewatford@gmail.com>
2020-04-27 19:43:58 +0200

    update lists.data to accept description

--- server/models/Project.js
+++ server/models/Project.js
@@ -6,7 +6,12 @@ const ProjectSchema = new Schema(
     name: { type: String, required: true, default: '' },
     admin: { type: String, required: true },
     members: [],
-    lists: [{ name: String, data: [{ name: String, member: String }] }],
+    lists: [
+      {
+        name: String,
+        data: [{ name: String, member: String, description: String }],
+      },
+    ],
     password: String,
   },
   {
