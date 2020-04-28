5822fb0a8aa1d67dedfc468e9a658440d6b3f993
Peupty <melvillewatford@gmail.com>
2020-04-28 22:20:03 +0200

    remove controller

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,5 @@ const router = require('express').Router();
 
 router.use('/users', require('./users'));
 router.use('/projects', require('./projects'));
-router.use('/fake', require('./fake'));
 
 module.exports = router;
--- server/controllers/fake/index.js
+++
@@ -1,8 +0,0 @@
-const Controller = require('./controller');
-
-const services = {
-  userService: require('../../services/user/index'),
-  projectService: require('../../services/project/index'),
-};
-
-module.exports = Controller(services);
