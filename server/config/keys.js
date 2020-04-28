7773b2745d06877b91e42ec888966d00a9ec6769
Peupty <melvillewatford@gmail.com>
2020-04-28 23:17:44 +0200

    update mongo key

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongodb:
-    "mongodb+srv://client:8774gpPNMhw4jHB@cluster0-clbgz.mongodb.net/test?retryWrites=true&w=majority"
+    'mongodb+srv://client:GQ4pigilAgJQlI7S@cluster0-clbgz.mongodb.net/test?retryWrites=true&w=majority',
 };
