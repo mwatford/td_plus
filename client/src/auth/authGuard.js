3d01af94757256dee7b484842a319327c7ba87f7
Peupty <melvillewatford@gmail.com>
2020-04-28 22:15:31 +0200

    change module to work with fake authentication

--- client/src/auth/authGuard.js
+++ client/src/auth/authGuard.js
@@ -1,27 +1,27 @@
-import { getInstance } from "./index";
+import { getInstance } from './index';
+import store from '../store/index';
 
 export const authGuard = (to, from, next) => {
-  const authService = getInstance();
+  // const authService = getInstance();
 
   const fn = () => {
     // If the user is authenticated, continue with the route
-    if (authService.isAuthenticated) {
+    if (store.state.auth.status) {
       return next();
     }
 
     // Otherwise, log in
-    next({ name: "start" });
+    next({ name: 'start' });
   };
 
   // If loading has already finished, check our auth state using `fn()`
-  if (!authService.loading) {
-    return fn();
-  }
+  // if (!authService.loading) {}
+  return fn();
 
   // Watch for the loading property to change before we check isAuthenticated
-  authService.$watch("loading", loading => {
-    if (loading === false) {
-      return fn();
-    }
-  });
+  // authService.$watch('loading', loading => {
+  //   if (loading === false) {
+  //     return fn();
+  //   }
+  // });
 };
