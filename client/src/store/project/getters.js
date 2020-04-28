5ebf6a9f6a57dafecaf4e8baae5a7f5355d5ce77
Peupty <melvillewatford@gmail.com>
2020-04-28 03:51:26 +0200

    add getTaskIndex

--- client/src/store/project/getters.js
+++ client/src/store/project/getters.js
@@ -1,1 +1,7 @@
-export const getters = {};
+export const getters = {
+  getTaskIndex: state => (listIndex, id) => {
+    const item = state.data.lists[listIndex].data.find(el => el._id === id);
+
+    return state.data.lists[listIndex].data.indexOf(item);
+  },
+};
