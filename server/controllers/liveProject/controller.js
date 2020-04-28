d6904b232fa36d0b2894f24ccbba8b6c38c02f7f
Peupty <melvillewatford@gmail.com>
2020-04-28 03:48:29 +0200

    refactor

--- server/controllers/liveProject/controller.js
+++ server/controllers/liveProject/controller.js
@@ -1,12 +1,10 @@
 module.exports = (services, manager) => http =>
   (async () => {
     try {
-      const io = require("socket.io")(http);
+      const io = require('socket.io')(http);
       const { projectService } = services;
 
-      io.on("connection", async socket => {
-        // console.log("user connected");
-
+      io.on('connection', async socket => {
         const project = await projectService.find(
           socket.handshake.query.project
         );
@@ -15,36 +13,20 @@ module.exports = (services, manager) => http =>
           el => el.id == socket.handshake.query.user
         );
 
-        const usera = require("../../modules/UserFactory")(user);
+        const chatUser = require('../../modules/UserFactory')(user);
 
-        manager.create(project.name);
+        manager.create(project._id);
 
-        socket.join(project.name);
-        manager.addUser(usera, project.name);
+        socket.join(project._id);
 
-        manager.get(project.name).users;
-        // room.addUser(usera);
-        socket.on("load messages", () => {
-          socket.emit("message", manager.get(project.name).messages);
-        });
+        chat({ socket, chatUser, user, io, manager, project });
+        manage({ socket, project, io, services });
 
-        socket.on("sendMessage", data => {
-          if (usera.write) {
-            // console.log(usera);
-            usera.write(data, manager.get(project.name));
-            console.log(manager.get(project.name).messages);
-            io.to(project.name).emit("message", {
-              user: user.id,
-              text: data
-            });
-          }
-        });
+        socket.on('disconnect', () => {
+          manager.get(project._id).removeUser(chatUser);
 
-
-        socket.on("disconnect", () => {
-          manager.get(project.name).removeUser(usera);
-          if (manager.get(project.name).empty) {
-            manager.delete(project.name);
+          if (manager.get(project._id).empty) {
+            manager.delete(project._id);
           }
         });
       });
@@ -53,8 +35,40 @@ module.exports = (services, manager) => http =>
     }
   })();
 
-// module.exports = (services, manager) => {
-//   return {
-//     init: init(services, manager)
-//   };
-// };
+const chat = ({ socket, chatUser, io, user, manager, project }) => {
+  manager.addUser(chatUser, project._id);
+
+  socket.on('load messages', () => {
+    socket.emit('message', manager.get(project._id).messages);
+  });
+
+  socket.on('sendMessage', data => {
+    if (chatUser.write) {
+      chatUser.write(data, manager.get(project._id));
+
+      io.to(project._id).emit('message', {
+        user: user.id,
+        text: data,
+      });
+    }
+  });
+};
+
+const manage = ({ socket, io, project, services }) => {
+  socket.on('user removed', id => {
+    io.to(project._id).emit('removed', id);
+  });
+
+  socket.on('project deleted', () => {
+    io.to(project._id).emit('project deleted');
+  });
+
+  socket.on('updated', async data => {
+    const { projectService } = services;
+
+    try {
+      await projectService.update(project._id, data);
+      io.to(project._id).emit('update project', data);
+    } catch (e) {}
+  });
+};
