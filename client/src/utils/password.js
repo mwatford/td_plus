3f3bfb47a39ba17969ae5e848b313ab616f89161
Peupty <melvillewatford@gmail.com>
2020-06-05 19:39:52 +0200

    refactor: change hashPassword to be async

--- client/src/utils/password.js
+++ client/src/utils/password.js
@@ -1,7 +1,8 @@
-import bcrypt from "bcryptjs";
+import bcrypt from 'bcryptjs';
 
-export const hashPassword = password => {
-  return bcrypt.hash(password, 10);
+export const hashPassword = async password => {
+  const hash = await bcrypt.hash(password, 10);
+  return hash;
 };
 
 export const comparePasswords = (test, password) => {
