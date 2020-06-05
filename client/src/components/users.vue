9647999062cae88bdeeeaff1a144adc4c0b2347a
Peupty <melvillewatford@gmail.com>
2020-06-05 19:13:36 +0200

    update dummy users with a sub property

--- client/src/components/users.vue
+++ client/src/components/users.vue
@@ -21,22 +21,27 @@ export default {
         {
           token: '11111',
           email: 'dummyuser1@asd.com',
+          sub: '11111',
         },
         {
           token: '22222',
           email: 'dummyuser2@asd.com',
+          sub: '22222',
         },
         {
           token: '33333',
           email: 'dummyuser3@asd.com',
+          sub: '33333',
         },
         {
           token: '44444',
           email: 'dummyuser4@asd.com',
+          sub: '44444',
         },
         {
           token: '55555',
           email: 'dummyuser5@asd.com',
+          sub: '55555',
         },
       ],
     };
