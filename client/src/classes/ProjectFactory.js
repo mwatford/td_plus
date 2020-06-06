d0115e55c8337af85364ac522acc89fc61511b32
Peupty <melvillewatford@gmail.com>
2020-06-06 15:14:47 +0200

    feat: add save, strip methods

--- client/src/classes/ProjectFactory.js
+++ client/src/classes/ProjectFactory.js
@@ -1,16 +1,29 @@
 export class Basic {
-  constructor({ name, admin, password }) {
+  constructor({ name, admin, password, members = [], lists, _id }) {
+    this._id = _id;
     this.type = 'basic';
     this.name = name;
     this.admin = admin;
-    this.members = [];
+    this.members = members;
     this.password = password;
-    this.lists = [
+    this.lists = lists || [
       { name: 'To Do', data: [] },
       { name: 'In Progress', data: [] },
       { name: 'Done', data: [] },
     ];
     this.timestamp = Date.now();
+  }
+  save(fn) {
+    return fn(this.strip());
+  }
+  strip() {
+    const copy = cloneDeep(this);
+    copy.members = this.members.map(el => ({
+      id: el.id,
+      role: el.role,
+      permissions: el.permissions,
+    }));
+    return copy;
   }
 }
 
@@ -18,7 +31,7 @@ export class Kanban extends Basic {
   constructor(data) {
     super(data);
     this.type = 'kanban';
-    this.lists = [
+    this.lists = data.lists || [
       { name: 'Backlog', data: [] },
       { name: 'Design', data: [] },
       { name: 'To Do', data: [] },
@@ -54,3 +67,5 @@ const factory = new Factory(templates);
 Object.freeze(factory);
 
 export default factory;
+
+const cloneDeep = x => JSON.parse(JSON.stringify(x));
