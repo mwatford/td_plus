ae93a9adc3b4c6392f1ad28478884a5f86ed256f
Peupty <melvillewatford@gmail.com>
2020-06-07 14:46:54 +0200

    refactor: replace request with api service call change validate method to resolve/reject

--- client/src/views/Create.vue
+++ client/src/views/Create.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="create row">
-    <form @submit.prevent class="box" ref="form">
+    <form @submit.prevent class="box" ref="create-form">
       <div class="col">
         <input
           type="text"
           name="name"
+          ref="name"
           placeholder="project name"
           v-model="name"
           class="input"
@@ -13,12 +14,14 @@
         <input
           v-if="auth"
           type="password"
+          ref="password"
           class="input"
           name="invite"
           v-model="password"
           placeholder="password (optional)"
         />
         <BaseSelect
+          ref="template-picker"
           :options="options"
           :placeholder="'Choose a template'"
           @change="v => (type = v)"
@@ -30,6 +33,7 @@
           class="button m-auto"
           @click.prevent="create"
           value="Create"
+          ref="submit"
         />
         <button class="button m-auto" @click.prevent="navigate(-1)">
           Back
@@ -47,6 +51,7 @@ import { hashPassword } from '../utils/password';
 import navigate from '../mixins/navigate';
 import factory from '../classes/ProjectFactory';
 import { manageProject } from '../services/LocalDbManager';
+import http from '../services/api/index';
 
 export default {
   mixins: [boxAnimations, navigate],
@@ -65,10 +70,12 @@ export default {
       auth: state => state.auth.status,
     }),
     options() {
-      return Object.keys(factory.templates).map(el => ({
-        value: el,
-        text: el,
-      }));
+      return (
+        Object.keys(factory.templates).map(el => ({
+          value: el,
+          text: el,
+        })) || []
+      );
     },
   },
   methods: {
@@ -76,35 +83,25 @@ export default {
       try {
         await this.validate();
 
-        const password = (await hashPassword(this.password)) || '';
+        const password = this.password ? await hashPassword(this.password) : '';
         this.project = factory.create(this.type, { name: this.name, password });
 
-        await this.saveProject();
-        if (this.auth) await this.updateUser();
+        if (this.auth) {
+          await http.projects.create(this.token, this.project);
+          await this.updateUser();
+        } else {
+          manageProject('add', this.project);
+        }
 
+        this.alert('success', 'Project created');
         this.navigate({ name: 'home' });
       } catch (e) {
-        this.alert('error', e);
+        this.alert('error', e || 'Something went wrong');
       }
     },
     async validate() {
-      return this.name && this.type;
-    },
-    saveProject() {
-      return this.auth ? this.request() : manageProject('add', this.project);
-    },
-    request() {
-      return this.$http({
-        method: 'post',
-        url: '/api/projects/create',
-        headers: {
-          Authorization: `Bearer ${this.token}`,
-          'Content-Type': 'application/json',
-        },
-        data: {
-          project: this.project,
-        },
-      });
+      if (!this.name || !this.type) return Promise.reject();
+      return Promise.resolve();
     },
     updateUser() {
       return this.$store.dispatch('user/fetchUser', {
