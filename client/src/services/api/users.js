4f455d7cfe4c6dd1b35b2ec00603eed28131630d
Peupty <melvillewatford@gmail.com>
2020-06-09 17:54:35 +0200

    feat: add methods: - updateUser - deleteUser - searchByEmail

--- client/src/services/api/users.js
+++ client/src/services/api/users.js
@@ -4,11 +4,42 @@ const fetchUser = http => token => {
     url: '/api/users/current',
     headers: {
       Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
     },
   });
 };
 
+const updateUser = http => (token, changes) =>
+  http({
+    method: 'put',
+    url: '/api/users/current',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    data: changes,
+  });
+
+const deleteUser = http => token =>
+  http({
+    method: 'delete',
+    url: '/api/users/current',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+const searchByEmail = http => (token, query) =>
+  http({
+    method: 'get',
+    url: `/api/users/search/${query}`,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
 export default http => ({
   fetchUser: fetchUser(http),
+  updateUser: updateUser(http),
+  deleteUser: deleteUser(http),
+  searchByEmail: searchByEmail(http),
 });
