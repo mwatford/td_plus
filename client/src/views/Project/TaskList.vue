b2ad3b65b151e869f9fa11eaa2c8f3bd99c51d9e
Peupty <melvillewatford@gmail.com>
2020-06-09 19:48:56 +0200

    feat: add methods: - editListName - deleteList

--- client/src/views/Project/TaskList.vue
+++ client/src/views/Project/TaskList.vue
@@ -1,10 +1,13 @@
 <template>
   <AppList :items="lists" :limit="9" header="Lists">
-    <template v-slot="{ item }">
-      <h3 slot="list-item-text" class="text">{{ item.name }}</h3>
+    <template v-slot="{ item, index }">
+      <h3 slot="list-item-text" class="text" @click="editListName(index)">
+        {{ item.name }}
+      </h3>
       <AppListItemAction
         title="delete list"
         slot="list-item-action"
+        @click.native="deleteList(index, item.name)"
       ></AppListItemAction>
     </template>
     <h3 slot="list-action" class="text text--center" @click="addList">
@@ -32,12 +35,29 @@ export default {
   methods: {
     ...mapMutations({ update: 'activeProject/UPDATE' }),
     addList() {
-      debugger;
-
       let name = prompt('Name:');
 
       if (name) {
         this.update({ fn: this.project.addList, data: { name: name } });
+        //save
+      }
+    },
+    editListName(index) {
+      let name = prompt('Change name:');
+
+      if (name && index) {
+        this.update({
+          fn: this.project.editListName,
+          data: { index, name },
+        });
+        //save
+      }
+    },
+    deleteList(index, name) {
+      let userResponse = confirm(`Are you sure you want to delete ${name}`);
+
+      if (userResponse && index) {
+        this.update({ fn: this.project.deleteList, data: { index } });
         //save
       }
     },
