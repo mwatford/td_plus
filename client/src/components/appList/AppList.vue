ccccceadd24ed6c5c416ad7d1fee834c43b0a893
Peupty <melvillewatford@gmail.com>
2020-06-09 19:49:52 +0200

    style: css changes

--- client/src/components/appList/AppList.vue
+++ client/src/components/appList/AppList.vue
@@ -1,10 +1,10 @@
 <template>
   <ul class="list">
-    <li v-if="header" class="list__header row">
+    <li v-if="header" class="list__item header row">
       <h3 class="text text--center">{{ header }}</h3>
     </li>
     <li v-for="(item, index) in items" :key="index" class="list__item">
-      <slot :item="item"></slot>
+      <slot :item="item" :index="index"></slot>
     </li>
     <li v-if="items.length < limit" class="list__item">
       <slot name="list-action"></slot>
@@ -41,38 +41,40 @@ export default {
   margin-left: 3px;
   list-style-type: none;
 
-  &__item,
-  &__header {
+  &__item {
     display: flex;
     background: #000000cc;
     border-radius: 2px;
     color: #fff;
-    margin-bottom: 3px;
     width: 250px;
     height: 43px;
     overflow: hidden;
+    margin-top: 3px;
+  }
 
-    &--red {
-      background: red;
+  .text {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+
+    &:hover {
+      background: #000000da;
     }
 
-    .text {
-      display: flex;
-      align-items: center;
-      padding: 0 12px;
-      width: 100%;
-      height: 100%;
-
-      &--center {
-        justify-content: center;
-      }
+    &--center {
+      justify-content: center;
     }
   }
-  // .text,
-  // li.add {
-  //   &:hover {
-  //     background: #000000da;
-  //   }
-  // }
+}
+.header {
+  margin-top: 0;
+
+  .text:hover {
+    cursor: initial;
+    background: initial;
+  }
 }
 </style>
