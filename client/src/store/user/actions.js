0701a96d87b5d7df8adf6295030a481841647aed
Peupty <melvillewatford@gmail.com>
2020-06-14 16:47:19 +0200

    rewrite actions using new http service

--- client/src/store/user/actions.js
+++ client/src/store/user/actions.js
@@ -1,37 +1,38 @@
-export const actions = http => {
-  return {
-    async fetchUser({ commit, dispatch }, { token }) {
-      try {
-        const { data } = await http.users.fetchUser(token);
-        if (data.message) {
-          dispatch('alerts/display', data.message, { root: true });
-        }
-        if (data.user) commit('SET_USER', data.user);
-      } catch (error) {
-        const { response } = error;
-        const { data } = response;
-        if (data.message) {
-          dispatch('alerts/display', data.message, { root: true });
-        }
-      }
-    },
-    resetState({ commit }) {
-      commit('RESET_STATE');
-    },
-    logout({ commit }) {
-      commit('RESET_STATE');
-    },
-    // remove
-    save({ commit }, { changes, token }) {
-      return http.projects.updateUser({
-        method: 'put',
-        url: '/api/users/current',
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-        data: changes,
-      });
-    },
-  };
+const fetchUser = http => async ({ commit, dispatch }, { token }) => {
+  try {
+    const { data } = await http.users.fetchUser(token);
+    if (data.message) {
+      dispatch('alerts/display', data.message, { root: true });
+    }
+    if (data.user) commit('SET_USER', data.user);
+  } catch (error) {
+    const { response } = error;
+    const { data } = response;
+    if (data.message) {
+      dispatch('alerts/display', data.message, { root: true });
+    }
+  }
 };
+
+const save = http => async ({ commit, dispatch }, { changes, token }) => {
+  try {
+    const { data } = await http.projects.updateUser(token, changes);
+
+    commit('SET_USER', data);
+    dispatch(
+      'alerts/display',
+      { type: 'success', message: 'Your name has been set' },
+      { root: true }
+    );
+  } catch (e) {
+    dispatch('alerts/display', {
+      type: 'success',
+      message: e || 'Error setting your name',
+    });
+  }
+};
+
+export const actions = http => ({
+  fetchUser: fetchUser(http),
+  save: save(http),
+});
