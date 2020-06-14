84f6d82bd7e5b27eae92c8267add8cceedca369c
Peupty <melvillewatford@gmail.com>
2020-06-14 19:33:34 +0200

    add aliases

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,22 @@
 // const ImageminPlugin = require("imagemin-webpack-plugin").default;
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
 
 module.exports = {
   entry: {
-    main: "./client/src/index.js"
+    main: './client/src/index.js',
     // vendor: "./src/vendor.js"
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ["html-loader"]
+        use: ['html-loader'],
       },
       {
         test: /\.vue$/,
-        use: ["vue-loader"]
-      }
+        use: ['vue-loader'],
+      },
       // {
       //   test: /\.(png|jpe?g|svg|gif)$/,
       //   use: {
@@ -27,15 +28,25 @@ module.exports = {
       //     }
       //   }
       // }
-    ]
+    ],
+  },
+  resolve: {
+    alias: {
+      Components: path.resolve(__dirname, 'client/src/components'),
+      Mixins: path.resolve(__dirname, 'client/src/mixins'),
+      Services: path.resolve(__dirname, 'client/src/services'),
+      Utils: path.resolve(__dirname, 'client/src/utils'),
+      Classes: path.resolve(__dirname, 'client/src/classes'),
+      Views: path.resolve(__dirname, 'client/src/views'),
+    },
   },
   plugins: [
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
     // new ImageminPlugin({
     //   test: /\.(jpe?g|png|svg|gif)$/,
     //   optipng: {
     //     optimizationLevel: 15
     //   }
     // })
-  ]
+  ],
 };
